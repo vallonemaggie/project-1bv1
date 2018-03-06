ad682b0bb6060fea9d581fd8f3542ff44dd66e85
vallonemaggie <vallonemaggie@gmail.com>
2018-03-05 18:45:17 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    
+    $("#overlay-show").click(function(){
+        $("#overlay").fadeIn(200);
+        
+        
+    });
+    
+    $("#overlay-close").click(function(){
+        $("#overlay").fadeOut(200);
+        
+    })
+    
+});
+
